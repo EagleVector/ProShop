fb19402cfaec8dc0b3bb7656c4eb99078fffe56d
EagleVector <kumarsubhambili3@gmail.com>
2023-08-13 18:58:34 +0530

    Order Routes Generation

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+const router = express.Router();
+import { 
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDeliver,
+  getOrders
+} from '../controllers/orderController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/')
+  .get(protect, admin, getOrders)
+  .post(protect, addOrderItems);
+
+router.route('/myorders')
+  .get(protect, getMyOrders);
+
+router.route('/:id')
+  .get(protect, admin, getOrderById);
+
+router.route('/:id/pay')
+  .put(protect, updateOrderToPaid);
+
+router.route('/:id/deliver')
+  .put(protect, admin, updateOrderToDeliver);
+
+export default router;
