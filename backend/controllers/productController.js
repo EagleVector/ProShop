8276a040494042076268d9e687dd9541948effab
EagleVector <kumarsubhambili3@gmail.com>
2023-08-18 13:31:52 +0530

    Add Review Backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -89,9 +89,54 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc        Create a new review
+// @route       POST /api/products/:id/reviews
+// @access      PRIVATE
+
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params._id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product Already Reviewed');
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating = 
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) / 
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: 'Review Added' });
+
+  } else {
+    res.status(404);
+    throw new Error('Resource not Found');
+  }
+});
+
 export { 
   getProducts, 
   getProductById, 
   createProduct, 
   updateProduct, 
-  deleteProduct };
+  deleteProduct,
+  createProductReview
+};
