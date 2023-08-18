7f1702e3a69b5f258aae4b7833fc1219aaf871a1
EagleVector <kumarsubhambili3@gmail.com>
2023-08-18 21:23:23 +0530

    Search Functionality backend and pagination for search

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -8,9 +8,12 @@ import Paginate from '../components/Paginate';
 
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams();
+  const { pageNumber, keyword } = useParams();
   
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+  const { data, isLoading, error } = useGetProductsQuery({ 
+    keyword, 
+    pageNumber 
+  });
 
   return (
     <>
@@ -32,7 +35,8 @@ const HomeScreen = () => {
         </Row>
         <Paginate 
           pages={data.pages}
-          page={data.page} />
+          page={data.page} 
+          keyword={keyword ? keyword : ''} />
           
         </>) }
     </>
